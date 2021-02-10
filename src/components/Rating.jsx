e3d77df027292ae403c8772a5349e759e132d94b
eddyeboy27 <lipe0503@gmail.com>
2021-02-10 04:53:07 -0300

    Novo readme e alterado repositório

---
+++ src/components/Rating.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <p className="rating">{this.props.rating}</p>
+    );
+  }
+}
+
+export default Rating;
